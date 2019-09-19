6e0b8ec4af7f54219b544868fa8162fd6ce2f727
Daniel Silva <danieldts2013@gmail.com>
2019-09-19 17:44:21 -0300

    alteracoes no view

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ApplicationRecord
   has_many :ratings, dependent: :destroy
   has_many :answers, through: :ratings
 
+  #controle para imagens
+
   validates :titulo, presence: true
 end
